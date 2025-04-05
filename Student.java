e1714fac33e7aafaa1d299fb8b281e5c1506fab8
anhltsefpt <anhltse.fpt@gmail.com>
2025-04-05 18:55:39 +0700

    Tạo class Student. Khởi tạo class Student

---
+++ Student.java
@@ -0,0 +1,17 @@
+class Student {
+    private String name;
+    private String id;
+    private String address;
+    private String phone;
+    private String email;
+    private String password;
+
+    Student(String name, String id, String address, String phone, String email, String password) {
+        this.name = name;
+        this.id = id;
+        this.address = address;
+        this.phone = phone;
+        this.email = email;
+        this.password = password;
+    }
+}
