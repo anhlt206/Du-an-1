058dcf648c3aec5f435c034763aa38e995a5a6be
anhltsefpt <anhltse.fpt@gmail.com>
2025-04-05 19:01:51 +0700

    Thêm tính năng Teacher

---
+++ Teacher.java
@@ -0,0 +1,13 @@
+class Teacher {
+    private String name;
+    private String id;
+    private String address;
+    private String phone;
+    private String email;
+    private String password;
+
+    Teacher(String name, String id, String address, String phone, String email, String password) {
+        this.name = name;
+        this.id = id;
+    }
+}
